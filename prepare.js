30165469747f94af5c82f9b23066d67af486d77f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-11-13 15:04:52 +1000

    yarn-add update script

--- prepare.js
+++ prepare.js
@@ -2,7 +2,8 @@ const { execSync } = require('child_process')
 
 if (process.env.NODE_ENV === 'production') {
   console.log('Installing deps')
+  execSync('yarn install')
+  console.log('Building Cohesive-UI')
+  execSync('yarn build')
 }
 
-console.log('Building Cohesive-UI')
-execSync('yarn build')
