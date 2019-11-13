4237be3493d1ad0aa2cd9c2d726b8753c8c5f615
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-11-13 16:32:44 +1000

    yarn-add add files key to package.json

--- prepare.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process')
-
-if (process.env.NODE_ENV === 'production') {
-  console.log('Installing deps')
-  execSync('yarn install')
-  console.log('Building Cohesive-UI')
-  execSync('yarn build')
-}
-
