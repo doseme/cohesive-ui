5755d001b56ceb684d26ec4ad41033af1098aeef
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-11-15 13:26:13 +1000

    remove duplicate

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { SideNav } from './SideNav'
 import { Navbar, INavLink } from './Navbar'
 import { Footer } from './Footer'
 import { Panel } from './Panel'
-import { Panel } from './Panel'
 import { SmartList } from './SmartList'
 
 export {
