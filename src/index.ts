72a37efc73fb95f322971666db9f62eb3cdd04f7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-11-27 14:52:50 +1000

    improve Update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Footer } from './Footer'
 import { PaginationPanel } from './PaginationPanel'
 import { Panel } from './Panel'
 import { SmartList } from './SmartList'
+import { List } from './List'
 import { Button } from './Button'
 import * as Nav from './Nav'
 
@@ -14,5 +15,6 @@ export {
   Button,
   Panel,
   Nav,
+  List,
   SmartList,
 }
