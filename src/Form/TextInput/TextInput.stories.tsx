0f25d2eb12c7ca97a8a9fc6a23176260cccf6c15
Julian Anning <julian.anning@gmail.com>
2019-11-29 16:24:51 +1000

    UI-9 oh come on

--- src/Form/TextInput/TextInput.stories.tsx
+++ src/Form/TextInput/TextInput.stories.tsx
@@ -8,7 +8,6 @@ storiesOf('Components.Form.TextInput', module)
   .add('Required input, 20 max length', () => {
     return (
       <div>
-        <div>This text field updates off focus. Changes are being logged to the console.</div>
         <TextInput
           label='This is an optional label'
           maxInputLength={20}
