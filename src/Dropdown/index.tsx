bee01cd121afe2820cd5749265a127a301a5610c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-12-09 13:52:11 +1000

    UI-8 init dropdown implementation

---
+++ src/Dropdown/index.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { InputGroup, FormControl, FormControlProps, } from 'react-bootstrap'
+
+import { Button } from '../Button'
+import './index.scss'
+import { ReplaceProps, BsPrefixProps } from 'react-bootstrap/helpers'
+
+interface IProps {
+  data?: string[]
+  title: string
+  onSelect?: (item: string) => void
+  searchIcon?: JSX.Element
+  showSearchThreshold?: number
+}
+
+const Dropdown: React.FC<IProps> = ({ children, data, title, searchIcon, onSelect, showSearchThreshold = 10 }) => {
+  const [showContent, setShowContent] = useState(false)
+  const [searchText, setSearchText] = useState('')
+  const [selectedItem, setSelectedItem] = useState('')
+  const node = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    document.addEventListener("mousedown", (e: MouseEvent) => {
+      handleClickAway(e)
+    })
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickAway)
+    }
+  }, [])
+
+  const handleClickAway = (e: MouseEvent) => {
+    // @ts-ignore - I dunno how to type this, whatever
+    if (node && node.current && node.current.contains(e.target)) {
+      return
+    }
+
+    setSearchText('')
+    setShowContent(false)
+  }
+
+  const handleChange = (e: React.FormEvent<ReplaceProps<'input', BsPrefixProps<'input'> & FormControlProps>>) => {
+    if (e.currentTarget.value) {
+      setSearchText(e.currentTarget.value.toLowerCase())
+      return
+    }
+    setSearchText('')
+  }
+
+  const searchInput = (
+    <InputGroup className='dropdown-search'>
+      <FormControl
+        placeholder='Search...'
+        aria-label='Search'
+        onChange={handleChange}
+      />
+      {
+        searchIcon && (
+          <InputGroup.Append>
+            <Button
+              className='h-100'
+              variant='light'
+              onClick={() => {/***/ }}
+            >
+              {searchIcon}
+            </Button>
+          </InputGroup.Append>
+        )
+      }
+    </InputGroup>
+  )
+
+  const listItems = (items: string[], callback: (item: string) => void) => (
+    <>
+      <div className='dropdown-title'>{title}</div>
+
+      {data && data.length > showSearchThreshold && searchInput}
+
+      <ul>
+        {
+          items.map(item =>
+            <li 
+              key={item}
+              onClick={() => {
+                  setSelectedItem(item)
+                  callback(item)
+                  setShowContent(false)
+                }
+              }
+            >
+              {item}
+            </li>
+          )
+        }
+      </ul>
+    </>
+  )
+
+  let filteredData = data;
+
+  if (data && searchText) {
+    filteredData = data.filter(x =>  x.toLowerCase().includes(searchText))
+  }
+
+  const content = showContent 
+    ? 
+    (
+      <div className='dropdown'>
+        {
+          filteredData && onSelect
+            ? listItems(filteredData, onSelect)
+            : children
+        }
+      </div>
+    ) 
+    : null
+
+  return (
+    <div 
+      className='dropdown-wrapper' 
+      ref={node}
+    >
+      <div
+        className='dropdown-closed'
+        onClick={() => setShowContent(!showContent)}
+      >
+        {selectedItem || title}
+      </div>
+      {content}
+    </div>
+  )
+}
+
+export {
+  Dropdown
+}
