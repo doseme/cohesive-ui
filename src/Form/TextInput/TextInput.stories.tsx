b32a00db4f557e44e4c19c5f90fde9f4904cb8b5
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-12-10 13:45:02 +1000

    Style TextInput

--- src/Form/TextInput/TextInput.stories.tsx
+++ src/Form/TextInput/TextInput.stories.tsx
@@ -7,7 +7,7 @@ import { TextInput } from './TextInput'
 storiesOf('Components.Form.TextInput', module)
   .add('Required input, 20 max length', () => {
     return (
-      <div>
+      <div className='m-4'>
         <TextInput
           label='This is an optional label'
           maxInputLength={20}
