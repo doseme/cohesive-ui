d1190f0f3ce7044bff908a22a580ce35e9492814
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-01-09 14:59:47 +1000

    AA-28 add basic context menu layout:

---
+++ src/ContextMenu/ContextMenu.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShare } from '@fortawesome/free-solid-svg-icons'
+
+import { ContextMenu, IListItem } from './ContextMenu'
+
+const stories = storiesOf('Components.ContextMenu', module)
+
+const icon = (
+  <FontAwesomeIcon
+    className='m-2'
+    icon={faShare}
+  />
+)
+
+const items: IListItem[] = [
+  {
+    id: 1,
+    icon,
+    text: 'Settings',
+  },
+  {
+    id: 2,
+    icon,
+    text: <div>Back to DoseMeRx</div>,
+  },
+]
+
+stories.add('ContextMenu', () => {
+  return (
+    <div className='m-5'>
+      <ContextMenu
+        header='Cohesive UI'
+        items={items}
+      />
+    </div>
+  )
+})
