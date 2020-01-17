802e64ff8a488170c354876a4470dac19be6c2a1
Julian Anning <julian.anning@gmail.com>
2020-01-17 10:33:41 +1000

    Fix busted date input validation

--- src/DateInput/validation.ts
+++ src/DateInput/validation.ts
@@ -11,14 +11,14 @@ export interface IValidationResult {
 }
 
 const validate = (rules: IValidators, text: string): IValidationResult => {
-  if (rules.isRequired && (!text.length || moment(text, 'DD-MM-YYYY').toString() === 'Invalid date')) {
+  if (rules.isRequired && (!text.length || moment(text, 'YYYY-MM-DD').toString() === 'Invalid date')) {
     return {
       message: 'This field is required',
       valid: false
     }
   }
 
-  if (rules.dateNotInPast && moment().startOf('day').isAfter(moment(text, 'DD-MM-YYYY').startOf('day'))) {
+  if (rules.dateNotInPast && moment().startOf('day').isAfter(moment(text, 'YYYY-MM-DD').startOf('day'))) {
     return {
       message: 'Date cannot be in the past',
       valid: false
