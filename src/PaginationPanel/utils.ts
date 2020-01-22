2fb737b6761a471d0ed3badfebc97c79506c7cc2
Julian Anning <julian.anning@gmail.com>
2020-01-23 09:09:09 +1000

    UI-15 The newline, the quote and the semicolon

--- src/PaginationPanel/utils.ts
+++ src/PaginationPanel/utils.ts
@@ -1,32 +1,30 @@
 import {
   INavigationNode,
   IPageNumberNode,
-  IPaginateOptions,
-  IPaginationState,
   TNode
-} from "./types";
+} from './types'
 
 const gotoStart: INavigationNode = {
-  type: "navigation",
-  symbol: "<<",
-  action: "gotoFirst"
-};
+  type: 'navigation',
+  symbol: '<<',
+  action: 'gotoFirst'
+}
 const previous: INavigationNode = {
-  type: "navigation",
-  symbol: "<",
-  action: "previous"
-};
+  type: 'navigation',
+  symbol: '<',
+  action: 'previous'
+}
 
 const gotoEnd: INavigationNode = {
-  type: "navigation",
-  symbol: ">>",
-  action: "gotoLast"
-};
+  type: 'navigation',
+  symbol: '>>',
+  action: 'gotoLast'
+}
 const next: INavigationNode = {
-  type: "navigation",
-  symbol: ">",
-  action: "next"
-};
+  type: 'navigation',
+  symbol: '>',
+  action: 'next'
+}
 
 const paginate = (
   currentPage: number,
@@ -34,59 +32,59 @@ const paginate = (
 ) => {
   const makePageNumberNode = (value: number): IPageNumberNode => {
     return {
-      type: "pageNumber",
+      type: 'pageNumber',
       value,
       isCurrentPage: value === currentPage
-    };
-  };
+    }
+  }
 
   // The total pages are less than 7 so no need to show '...'.
   // Just render all pages.
   if (pageCount <= 7) {
-    const nodes: TNode[] = [gotoStart, previous];
+    const nodes: TNode[] = [gotoStart, previous]
     for (let i = 0; i < pageCount; i++) {
-      nodes.push(makePageNumberNode(i + 1));
+      nodes.push(makePageNumberNode(i + 1))
     }
-    return [...nodes, next, gotoEnd];
+    return [...nodes, next, gotoEnd]
   }
 
   // current page is near the start
   if (currentPage < 3) {
-    const nodes: TNode[] = [gotoStart, previous];
-    nodes.push(makePageNumberNode(1));
-    nodes.push(makePageNumberNode(2));
-    nodes.push(makePageNumberNode(3));
-    nodes.push({ type: "dots", value: "..." });
-    nodes.push(makePageNumberNode(pageCount - 2));
-    nodes.push(makePageNumberNode(pageCount - 1));
-    nodes.push(makePageNumberNode(pageCount));
-    return [...nodes, next, gotoEnd];
+    const nodes: TNode[] = [gotoStart, previous]
+    nodes.push(makePageNumberNode(1))
+    nodes.push(makePageNumberNode(2))
+    nodes.push(makePageNumberNode(3))
+    nodes.push({ type: 'dots', value: '...' })
+    nodes.push(makePageNumberNode(pageCount - 2))
+    nodes.push(makePageNumberNode(pageCount - 1))
+    nodes.push(makePageNumberNode(pageCount))
+    return [...nodes, next, gotoEnd]
   }
 
   // current page is near the end
   if (currentPage > pageCount - 5) {
-    const nodes: TNode[] = [gotoStart, previous];
-    nodes.push(makePageNumberNode(1));
-    nodes.push({ type: "dots", value: "..." });
-    nodes.push(makePageNumberNode(pageCount - 4));
-    nodes.push(makePageNumberNode(pageCount - 3));
-    nodes.push(makePageNumberNode(pageCount - 2));
-    nodes.push(makePageNumberNode(pageCount - 1));
-    nodes.push(makePageNumberNode(pageCount));
-    return [...nodes, next, gotoEnd];
+    const nodes: TNode[] = [gotoStart, previous]
+    nodes.push(makePageNumberNode(1))
+    nodes.push({ type: 'dots', value: '...' })
+    nodes.push(makePageNumberNode(pageCount - 4))
+    nodes.push(makePageNumberNode(pageCount - 3))
+    nodes.push(makePageNumberNode(pageCount - 2))
+    nodes.push(makePageNumberNode(pageCount - 1))
+    nodes.push(makePageNumberNode(pageCount))
+    return [...nodes, next, gotoEnd]
   }
 
-  // current page is "in the middle", eg not near the start or end!!
-  const nodes: TNode[] = [gotoStart, previous];
-  nodes.push(makePageNumberNode(1));
-  nodes.push({ type: "dots", value: "..." });
-  nodes.push(makePageNumberNode(currentPage - 1));
-  nodes.push(makePageNumberNode(currentPage));
-  nodes.push(makePageNumberNode(currentPage + 1));
-  nodes.push({ type: "dots", value: "..." });
-  nodes.push(makePageNumberNode(pageCount));
-  return [...nodes, next, gotoEnd];
-};
+  // current page is 'in the middle', eg not near the start or end!!
+  const nodes: TNode[] = [gotoStart, previous]
+  nodes.push(makePageNumberNode(1))
+  nodes.push({ type: 'dots', value: '...' })
+  nodes.push(makePageNumberNode(currentPage - 1))
+  nodes.push(makePageNumberNode(currentPage))
+  nodes.push(makePageNumberNode(currentPage + 1))
+  nodes.push({ type: 'dots', value: '...' })
+  nodes.push(makePageNumberNode(pageCount))
+  return [...nodes, next, gotoEnd]
+}
 
 export {
   paginate,
