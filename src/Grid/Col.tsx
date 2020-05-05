1a100c4aa772a20661c329874fc99c358d7caa17
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-05 12:07:19 +1000

    UI-26 Update grid

--- src/Grid/Col.tsx
+++ src/Grid/Col.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 interface IProps {
   width?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
   className?: string
+  onClick?: () => any
 }
 
 const Col: React.FC<IProps> = (props) => {
+  const { className: _, ...rest } = props
   const className = props.className 
     ? `${props.className} co-col co-col-${props.width}` 
     : `co-col co-col-${props.width}`
 
-  return <div className={className}>{props.children}</div>
+  return <div {...rest} className={className}>{props.children}</div>
 }
 
 export {
