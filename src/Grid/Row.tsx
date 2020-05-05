bbe7e03b7f72ee2b8b3ec471497cbdd6bf61ff96
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-05 11:11:01 +1000

    UI-26 Update styling

--- src/Grid/Row.tsx
+++ src/Grid/Row.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { HTMLProps } from 'react'
 
-interface IProps {
+interface IProps extends HTMLProps<HTMLDivElement> {
   className?: string
 }
 
 const Row: React.FC<IProps> = (props) => {
+  const { className: _, ...rest } = props
   const className = props.className ? `${props.className} co-row` : 'co-row'
-  return <div className={className}>{props.children}</div>
+  return <div className={className} {...rest}>{props.children}</div>
 }
 
 export {
