09fcbcab465541a89ec90dfd752cbb45a31b169d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-11 18:47:18 +1000

    build-step build step

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,41 @@
 import typescript from 'rollup-plugin-typescript2'
 import sass from 'rollup-plugin-sass'
 
-export default {
-  input: './src/index.ts',
-  output: {
-    file: './dist/index.js',
-    format: 'es',
-  },
-  external: [
-    'react',
-    'react-bootstrap',
-    'react-bootstrap/Row',
-    'react-bootstrap/Col',
-    'react-bootstrap/ListGroup',
-    '@fortawesome/react-fontawesome',
-    '@fortawesome/free-solid-svg-icons',
-    'react-bootstrap/Form',
-    'react-bootstrap/InputGroup',
-    'react-bootstrap/Button',
-    'react-bootstrap/OverlayTrigger',
-    'react-bootstrap/Tooltip',
-    'react-bootstrap/Navbar',
-    'moment',
-  ],
-  plugins: [
-    typescript(),
-    sass({
-      output: true
-    })
-  ]
+const createEntry = ({ format, file }) => {
+  const config = {
+    input: './src/index.ts',
+    output: {
+      file: './dist/' + file,
+      format,
+    },
+    external: [
+      'react',
+      'react-bootstrap',
+      'react-bootstrap/Row',
+      'react-bootstrap/Col',
+      'react-bootstrap/ListGroup',
+      '@fortawesome/react-fontawesome',
+      '@fortawesome/free-solid-svg-icons',
+      'react-bootstrap/Form',
+      'react-bootstrap/InputGroup',
+      'react-bootstrap/Button',
+      'react-bootstrap/OverlayTrigger',
+      'react-bootstrap/Tooltip',
+      'react-bootstrap/Navbar',
+      'moment',
+    ],
+    plugins: [
+      typescript(),
+      sass({
+        output: true
+      })
+    ]
+  }
+
+  return config
 }
+
+export default [
+  createEntry({ format: 'es', file: 'cohesive-ui.esm.js' }),
+  createEntry({ format: 'cjs', file: 'cohesive-ui.cjs.js' }),
+]
