f75aee2c84623b0ce3912380e99cf8029ecffcf9
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-18 14:01:14 +1000

    UI-34 add basic toggle component

---
+++ src/Toggle/Toggle.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import './index.scss'
+
+export interface IToggleOption {
+  id: string
+  value: string
+  label: string
+  testid?: string
+}
+
+type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U;
+interface IProps extends React.HTMLAttributes<HTMLElement> {
+  selected: string
+  name: string
+  options: IToggleOption[]
+}
+type TProps = Overwrite<IProps, {
+  onChange: (option: IToggleOption) => any
+}>
+
+const Toggle: React.FC<TProps> = (props): JSX.Element => {
+  if (props.selected) {
+    console.warn(`<Toggle /> requires a \`selected\` prop.`)
+  }
+
+  const handleChange = (event: React.FormEvent<HTMLDivElement>) => {
+    const selectedOption = props.options.find(x => x.id === event.currentTarget.id)!
+    props.onChange(selectedOption)
+  }
+
+  const radioButtons: JSX.Element[] = props.options.map(x =>
+    <>
+      <label 
+        htmlFor={x.id}
+        className={x.id === props.selected ? 'co-toggle-label co-toggle-label-selected' : 'co-toggle-label'}
+      >
+        {x.label}
+      </label>
+      <input 
+        type='radio' 
+        onChange={handleChange}
+        name={props.name}
+        checked={x.value === props.selected}
+        id={x.id}
+        value={x.value}
+        data-testid={x.testid}
+      />
+    </>
+  )
+
+  const className = `co-toggle-group ${props.className}`
+  return (
+    <div className={className} id={props.id} style={props.style}>
+      {radioButtons}
+    </div>
+  )
+}
+
+
+export { Toggle }
