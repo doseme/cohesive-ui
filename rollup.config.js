e40e30f890c7c4723f0b2636a51079a9709578b7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-20 16:37:01 +1000

    UI-30 Update dependencies and build step

--- rollup.config.js
+++ rollup.config.js
@@ -10,18 +10,8 @@ const createEntry = ({ format, file }) => {
     },
     external: [
       'react',
-      'react-bootstrap',
-      'react-bootstrap/Row',
-      'react-bootstrap/Col',
-      'react-bootstrap/ListGroup',
       '@fortawesome/react-fontawesome',
       '@fortawesome/free-solid-svg-icons',
-      'react-bootstrap/Form',
-      'react-bootstrap/InputGroup',
-      'react-bootstrap/Button',
-      'react-bootstrap/OverlayTrigger',
-      'react-bootstrap/Tooltip',
-      'react-bootstrap/Navbar',
       'moment',
     ],
     plugins: [
