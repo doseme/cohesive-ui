b801ac696228d659d94135182719ab9c49a472ed
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-06-08 16:53:06 +1000

    remove ignore

--- src/Icons/DoseMe.tsx
+++ src/Icons/DoseMe.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const DoseMe: React.FC = () => {
   return (
-    // @ts-ignore
-    <svg className='co-doseme-icon' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
+    <svg className='co-doseme-icon' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" href="http://www.w3.org/1999/xlink">
       <rect width="32" height="32" rx="16" fill="url(#pattern0)" />
       <defs>
         <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
