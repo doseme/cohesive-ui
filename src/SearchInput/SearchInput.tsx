dbe73aa93c7a7931d7e60ddbf92f88ed24bf31f8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-06-11 16:34:11 +1000

    do not prefill search

--- src/SearchInput/SearchInput.tsx
+++ src/SearchInput/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
@@ -13,6 +13,7 @@ interface IProps {
 }
 
 const SearchInput: React.FC<TExtendsHTMLElement<IProps>> = (props): JSX.Element => {
+  const input = useRef<HTMLInputElement>(null)
   const handleChange = (e: React.FormEvent<any>) => {
     const value = e.currentTarget && e.currentTarget.value
     props.onChange(value || '')
@@ -25,10 +26,18 @@ const SearchInput: React.FC<TExtendsHTMLElement<IProps>> = (props): JSX.Element 
     />
   )
 
+  const onFocus = () => {
+    input.current?.removeAttribute('readonly')
+  }
+
   return (
     <div className='search-outer'>
       <div className='search-wrapper'>
         <input
+          ref={input}
+          readOnly={true}
+          autoComplete='none'
+          onFocus={onFocus}
           data-testid='search-input'
           placeholder={props.placeholder || 'Search'}
           className='smart-list-search'
