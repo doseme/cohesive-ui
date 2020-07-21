cd620df35c2c083db1df334af6620c2b85b2d286
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-07-21 18:49:41 +1000

    UI-51 style

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -4,7 +4,9 @@ export interface IconProps {
 
 export * from './ThinSpinner'
 export * from './Final/Archive'
+export * from './Archive'
 export * from './Final/Unarchive'
 export * from './Final/Edit'
+export * from './Final/Delete'
 export * from './Trash'
 export * from './TrashFill'
