608db23390daa8fa5e043348a5eda6eadf0f9b74
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-03 11:42:21 +1000

    update stroke-width to be camel case

--- src/Icons/Schedule/Schedule.tsx
+++ src/Icons/Schedule/Schedule.tsx
@@ -7,8 +7,8 @@ interface IProps {
 const Schedule: React.FC<IProps> = (props) => {
   return (
     <svg width='11' height='11' viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <rect x='1' y='1' width='9' height='9' stroke={props.stroke} stroke-width='2' />
-      <line x1='4' y1='4' x2='7' y2='4' stroke={props.stroke} stroke-width='2' />
+      <rect x='1' y='1' width='9' height='9' stroke={props.stroke} strokeWidth='2' />
+      <line x1='4' y1='4' x2='7' y2='4' stroke={props.stroke} strokeWidth='2' />
     </svg>
   )
 }
