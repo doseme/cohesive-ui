921458ca1751425cee485219d8065d40d71e69b0
Julian Anning <julian.anning@gmail.com>
2021-02-16 13:29:23 +1000

    UI-67 this change was not needed so revert

--- src/InfoModal/InfoModal.tsx
+++ src/InfoModal/InfoModal.tsx
@@ -12,7 +12,6 @@ interface IProps {
   message: string | JSX.Element
   linkComponent?: JSX.Element
   linkHref?: string
-  linkDisabled?: boolean
   linkLabel?: string
   onDismiss?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => any
 }
@@ -27,9 +26,8 @@ const InfoModal: React.FC<IProps> = (props) => {
       return (
         <a className='info-link' href={props.linkHref} target='_blank'>
           <Button
-            variant='info'
-            className='info-button'
-            disabled={!!props.linkDisabled}
+              variant='info'
+              className='info-button'
           >
             {props.linkLabel}
           </Button>
