0bf93ee7f52ad6800bd8a137778297abb416bf78
Julian Anning <julian.anning@gmail.com>
2021-03-04 12:52:50 +1000

    UI-68 create new files

--- src/InputEditButton/InputEditButton.tsx
+++ src/InputEditButton/InputEditButton.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
+<<<<<<< Updated upstream
+=======
+import { Edit } from '../Icons'
+>>>>>>> Stashed changes
 
 import './index.scss'
 
 interface IProps {
   displayText: string
+<<<<<<< Updated upstream
+=======
+  onClick?: () => void
+>>>>>>> Stashed changes
 }
 
 export const InputEditButton: React.FC<IProps> = (props) => {
   return (
+<<<<<<< Updated upstream
     <div>
     </div>
+=======
+    <button
+      className='inputedit-btn'
+      onClick={props.onClick}
+    >
+      <Edit />
+      <span>{props.displayText}</span>
+    </button>
+>>>>>>> Stashed changes
   )
 }
