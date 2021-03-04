d86a09aa307cd471b13ec40525a45522be11abbb
Julian Anning <julian.anning@gmail.com>
2021-03-04 12:57:33 +1000

    UI-68 remove merge conflict markers

--- src/InputEditButton/InputEditButton.tsx
+++ src/InputEditButton/InputEditButton.tsx
@@ -1,25 +1,15 @@
 import React from 'react'
-<<<<<<< Updated upstream
-=======
 import { Edit } from '../Icons'
->>>>>>> Stashed changes
 
 import './index.scss'
 
 interface IProps {
   displayText: string
-<<<<<<< Updated upstream
-=======
   onClick?: () => void
->>>>>>> Stashed changes
 }
 
 export const InputEditButton: React.FC<IProps> = (props) => {
   return (
-<<<<<<< Updated upstream
-    <div>
-    </div>
-=======
     <button
       className='inputedit-btn'
       onClick={props.onClick}
@@ -27,6 +17,5 @@ export const InputEditButton: React.FC<IProps> = (props) => {
       <Edit />
       <span>{props.displayText}</span>
     </button>
->>>>>>> Stashed changes
   )
 }
