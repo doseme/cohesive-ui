2245174c3f9fda6d34ecbbe2b923f2daa9d17490
Julian Anning <julian.anning@gmail.com>
2021-03-08 08:54:09 +1000

    UI-68 Add story with modal hook

--- src/InputEditButton/InputEditButton.stories.tsx
+++ src/InputEditButton/InputEditButton.stories.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
+
 import { InputEditButton } from './InputEditButton'
+import { Modal } from '../Modal'
+import { InfoModal } from '../InfoModal'
 
 storiesOf('Components.InputEditButton', module)
-  .add('InputEditButton', () => {
+  .add('InputEditButton states', () => {
     return (
       <React.Fragment>
         <h1>Input Edit Button with Hover</h1>
@@ -20,5 +23,44 @@ storiesOf('Components.InputEditButton', module)
           edited
         />
       </React.Fragment>
+    )
+  })
+
+storiesOf('Components.InputEditButton', module)
+  .add('InputEditButton with modal hook', () => {
+    const Comp = () => {
+      const [show, setShow] = useState(false)
+
+      const MessageContent: React.FC = () => {
+        return (
+          <div>
+            This is a test
+          </div>
+        )
+      }
+
+      return (
+        <>
+          <Modal show={show} onHide={() => setShow(false)}>
+            <InfoModal
+              title='Update Value'
+              linkLabel='Update'
+              message={<MessageContent />}
+              onDismiss={() => setShow(false)}
+            />
+          </Modal>
+
+          <h1>Input Edit Button with Modal Hook</h1>
+          <br/>
+          <InputEditButton
+            onClick={() => setShow(true)}
+            displayText='1 hour'
+          />
+        </>
+      )
+    }
+
+    return (
+      <Comp />
     )
   })
